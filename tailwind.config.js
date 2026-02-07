0a84aff124aa5f17e084491a5ed5e34552d72590
Alexander Kens <alexkens93@gmail.com>
2026-02-07 15:01:49 +0100

    intro

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"], // adjust paths
+    theme: {
+        extend: {},
+        plugins: [],
+    }
+};
